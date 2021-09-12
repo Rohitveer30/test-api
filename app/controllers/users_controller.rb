6db677613c0ee3f4d89843d38b343370a109f6b5
sanketveer <sanketveer9612@gmail.com>
2021-09-12 13:40:00 +0530

    test api

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+
+	def typeahead
+    search_key = params[:term]&.split("")
+    result = []
+    search_key.each do |key|
+		  result << User.where('first_name like ?', "%#{key}%").or(User.where('last_name like ?', "%#{key}%").or(User.where('email like ?', "%#{key}%")))
+		end
+    result.compact!
+    res = result.reject { |c| c.empty? }
+    res.flatten!
+    render json: res.uniq
+	end
+
+
+
+	def index
+		@user = User.order('created_at DESC').limit(3)
+		render json: @user
+	end
+
+	def show
+		@user = User.find_by(id: params[:id])
+    if @user
+      render json: @user
+    else  
+      message = "No record was found for id #{params[:id]}."
+      render  json: { error: message }, status: :not_found
+    end
+	end
+
+	def create
+		@user = User.create!(user_params)
+		render json: @user, status: :created
+	end
+
+	def update
+		@user = User.find_by(id: params[:id])
+		if @user
+      @user.update(user_params)
+  		render json: @user, status: :updated
+    else
+      message = "No record was found for id #{params[:id]}."
+      render  json: { error: message }, status: :not_found
+    end
+	end
+
+
+	def delete
+		@user = User.find_by(id: params[:id])
+		if @user
+      @user.delete 
+  		render json: {}
+    else
+      message = "No record was found for id #{params[:id]}."
+      render  json: { error: message }, status: :not_found
+    end
+	end
+
+	private
+
+	def user_params
+		params.permit(:first_name, :last_name, :email)
+	end
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  scope '/api', format: :json do
+    get 'users', to: 'users#index'
+    get '/user/:id', to: 'users#show'
+    delete 'user/:id', to: 'users#delete'
+    put 'user/:id', to: 'users#update'
+    post 'user', to: 'users#create'
+    get 'user/typeahead/:term', to: 'users#typeahead'
+  end
+end
